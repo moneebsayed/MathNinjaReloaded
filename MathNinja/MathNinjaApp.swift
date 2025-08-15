9f09ca6852ec62d1845877748e48137aa5ec8297
Moneeb S. Sayed <moneebsayed@yahoo.com>
2025-08-15 11:46:40 -0400

    Initial Commit

---
+++ MathNinja/MathNinjaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathNinjaApp.swift
+//  MathNinja
+//
+//  Created by Moneeb Sayed on 8/15/25.
+//
+
+import SwiftUI
+
+@main
+struct MathNinjaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MathNinjaTests/MathNinjaTests.swift
@@ -0,0 +1,17 @@
+//
+//  MathNinjaTests.swift
+//  MathNinjaTests
+//
+//  Created by Moneeb Sayed on 8/15/25.
+//
+
+import Testing
+@testable import MathNinja
+
+struct MathNinjaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
